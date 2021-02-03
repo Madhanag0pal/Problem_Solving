beb9e8fd7891f210f64c4f37da0b02feaf7f6dfc
Madhanag0pal <72132615+Madhanag0pal@users.noreply.github.com>
2021-02-03 14:21:42 +0530

    Delete A Didderent Problem directory

--- A Didderent Problem/README.md
+++
@@ -1,3 +0,0 @@
-# [A Didderent Problem](https://open.kattis.com/problems/different)
-
-![A-Didderent-Problem](/A Didderent Problem/different.png)
--- A Didderent Problem/different.py
+++
@@ -1,9 +0,0 @@
-f = True
-while f:
-    try:
-        l = list(map(lambda a:int(a),input().split()))
-    except EOFError as e:
-        f = False
-        continue
-    # print(l[0]-l[1] if l[0] > l[1] else l[1] - l[0])
-    print(abs(l[0] - l[1]))
